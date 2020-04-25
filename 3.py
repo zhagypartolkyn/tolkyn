a3cd81794594f51e2633c651047d31f1a7cf9f43
zhagypartolkyn <60309067+zhagypartolkyn@users.noreply.github.com>
2020-04-25 08:18:20 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,6 @@
 def square1(A,ind):
-    
     for i in range (ind, len(A)):
         A[i]=A[i]**2
-    return A
+    return B
 print(square1([10, 3, 4, 5, 9], 2))
 print(square1([9, 0, 12, 3, 4, 56, 9, 16], 6))
