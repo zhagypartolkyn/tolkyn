35122c86cbf396800db6cc1a4b628da0553e4828
zhagypartolkyn <60309067+zhagypartolkyn@users.noreply.github.com>
2020-04-26 20:02:01 +0300

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-def list_2(A):
-    b=[]
-    for i in range(len(A)):
-        if i%2==0:
-            b.append(A[i])
-    return b
-print(list_2([10, 3, 4, 5, 9]))
-print(list_2([9, 0, 12, 3, 4, 56, 9, 16]))
