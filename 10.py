64a2cda36db1b8c1ec29274d738a3462063df399
zhagypartolkyn <60309067+zhagypartolkyn@users.noreply.github.com>
2020-04-26 20:00:57 +0300

    Delete 10.py

--- 10.py
+++
@@ -1,8 +0,0 @@
-def exercise_10(s):
-    s1=set()
-    for i in s:
-        s1.add(i-1)
-        s1.add(i+1)
-    return s1    
-print (exercise_10({1, 5, 9}))
-print (exercise_10({2, 5, 7, 8, 10}))
