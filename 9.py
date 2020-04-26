819215bcd1088fc63fb76d9d9127cad3ff04ce63
zhagypartolkyn <60309067+zhagypartolkyn@users.noreply.github.com>
2020-04-26 20:02:58 +0300

    Delete 9.py

--- 9.py
+++
@@ -1,9 +0,0 @@
-def mute(x):
-y=list(x)
-if tuple1(list[x]):
-    return True
-else:
-    return False   
-
-print(mute(tuple1(10, 2, 5, [4, 8, 2], 3, 5)))
-print(mute(tuple1(5, 2.5, 8, 4, 'Hi', -5, True, 6)))
